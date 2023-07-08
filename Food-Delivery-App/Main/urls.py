d791c71c9a048f364b2a7dfbbd278d04f3aa6458
srosama <srosamazaid@gmail.com>
2023-07-08 15:01:41 +0300

    Update

--- Food-Delivery-App/Main/urls.py
+++ Food-Delivery-App/Main/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
+
+    path('',  TemplateView.as_view(template_name='base.html')),
     path('admin/', admin.site.urls),
+    path('auth/', include('AuthAuthorization.urls')),
+
 ]
