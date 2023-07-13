f23c3a3d80f92712786f271d4ee4258e21fc01ac
Eslamzaid <113968253+Eslamzaid@users.noreply.github.com>
2023-07-13 13:52:12 +0300

    EslamBransh

--- Food-Delivery-App/AuthAuthorization/forms.py
+++ Food-Delivery-App/AuthAuthorization/forms.py
@@ -18,20 +18,6 @@ from django.utils.translation import gettext_lazy as _
 UserModel = get_user_model()
 
 
-def _unicode_ci_compare(s1, s2):
-    """
-    Perform case-insensitive comparison of two identifiers, using the
-    recommended algorithm from Unicode Technical Report 36, section
-    2.11.2(B)(2).
-    """
-    return (
-        unicodedata.normalize("NFKC", s1).casefold()
-        == unicodedata.normalize("NFKC", s2).casefold()
-    )
-
-
-
-
 class PasswordResetForm(forms.Form):
     email = forms.EmailField(
         max_length=254,
--- Food-Delivery-App/Main/settings.py
+++ Food-Delivery-App/Main/settings.py
@@ -82,16 +82,11 @@ WSGI_APPLICATION = 'Main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'foodAppV2',
-        'HOST':'127.0.0.1',
-        'PORT':'3306',
-        'USER':'root',
-        'PASSWORD':'osama12'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
+ 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
